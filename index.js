b1e5beb0a737ddfe47e6510a552d062db97cecb5
Juan <ssjmatos@gmail.com>
2022-01-13 13:19:35 -0500

    commit tree 0cc23c3070604c3605baff69be6e27066b9accf1

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+function upperCaseCustomerName () {
+    return customerName.toUpperCase();
+}
+
+function setBestCustomer () {
+    bestCustomer = "not bob";
+   // return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "jason";
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = "not jason"
+}
